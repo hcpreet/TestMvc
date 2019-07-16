62554416464960067898eed671c50da829da91b6
Harpreet <hcpreet@gmail.com>
2019-07-16 21:00:10 +1000

    My first Commit

---
+++ Controllers/InfoesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebNextData.Models;
+using PagedList;
+
+
+namespace WebNextData.Controllers
+{
+    public class InfoesController : Controller
+    {
+        private NextDataEntities db = new NextDataEntities();
+
+        // GET: Infoes
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString ,int? page)
+        {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+            var Infoes = from s in db.Infoes
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                Infoes = Infoes.Where(s => s.FirstName.Contains(searchString)
+                                       || s.LastName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    Infoes = Infoes.OrderByDescending(s => s.FirstName);
+                    break;
+                default:
+                    Infoes = Infoes.OrderBy(s => s.FirstName);
+                    break;
+            }
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(Infoes.ToPagedList(pageNumber, pageSize));
+
+            
+        }
+
+        // GET: Infoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Info info = db.Infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // GET: Infoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Infoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailId")] Info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Infoes.Add(info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(info);
+        }
+
+        // GET: Infoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Info info = db.Infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: Infoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailId")] Info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(info);
+        }
+
+        // GET: Infoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Info info = db.Infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: Infoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Info info = db.Infoes.Find(id);
+            db.Infoes.Remove(info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebNextData.Startup))]
+namespace WebNextData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
